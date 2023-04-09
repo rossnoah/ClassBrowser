c0b624f93acc9f64e22aa61a5a37be48d17a4287
Noah Ross <124201966+rossnoah@users.noreply.github.com>
2023-04-09 14:39:57 -0400

    added up arrow in corner

--- src/routes/classes/+page.svelte
+++ src/routes/classes/+page.svelte
@@ -449,3 +449,11 @@
 {/each}
 
 
+
+<button class="fixed bottom-0 right-0 p-3 m-2 bg-slate-400 text-white rounded-full shadow-lg"
+  on:click={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+  <i class="fa fa-arrow-up"></i> <!-- Font Awesome icon -->
+  <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"> <!-- Heroicons icon -->
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
+  </svg>
+</button>
